254a70d2ea7c792b996621aee41b8ca51408f88a
Asif Mahmud <asif01050105@gmail.com>
2021-01-25 11:17:48 +0600

    atlas integrated

--- config/config.js
+++ config/config.js
@@ -1,12 +1,29 @@
+// const config = {
+//   env: process.env.NODE_ENV || 'development',
+//   port: process.env.PORT || 3000,
+//   jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
+//   mongoUri: process.env.MONGODB_URI ||
+//     process.env.MONGO_HOST ||
+//     'mongodb://' + (process.env.IP || 'localhost') + ':' +
+//     (process.env.MONGO_PORT || '27017') +
+//     '/mernproject'
+// }
+
+
 const config = {
-  env: process.env.NODE_ENV || 'development',
-  port: process.env.PORT || 3000,
-  jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
-  mongoUri: process.env.MONGODB_URI ||
-    process.env.MONGO_HOST ||
-    'mongodb://' + (process.env.IP || 'localhost') + ':' +
-    (process.env.MONGO_PORT || '27017') +
-    '/mernproject'
-}
+	env: process.env.NODE_ENV || 'development',
+	port: process.env.PORT || 3000,
+	jwtSecret: process.env.JWT_SECRET || 'YOUR_secret_key',
+	mongoUri:
+		process.env.MONGODB_URI ||
+		process.env.MONGO_HOST ||
+		'mongodb+srv://mirsahib:mirsahib@mern-auth.bjobf.mongodb.net/mern-auth?retryWrites=true&w=majority',
+};
+
+
+//  mongodb+srv://mirsahib:mirsahib@mern-auth.bjobf.mongodb.net/mern-auth?retryWrites=true&w=majority
+
 
 export default config
+
+
